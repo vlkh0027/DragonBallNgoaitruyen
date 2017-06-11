3aa2b1ceaee5bb28b5a5f06a68164dab99a57d6b
Mouse <nlinh20011995@gmail.com>
2017-06-10 22:56:25 -0700

    ver1

---
+++ app/src/main/java/com/example/mouse/dragonballngoaitruyen/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.example.mouse.dragonballngoaitruyen;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mouse on 6/10/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
